9cfb3c5a01b710cc47205f181d7666a9b90426fd
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-01-21 13:27:28 +0000

    visual changes for the library

--- src/index.mjs
+++ src/index.mjs
@@ -10,6 +10,7 @@ const addBookPages = document.querySelector("#pages")
 const addBookStatus = document.querySelector("#status")
 const addBookCoverimage = document.querySelector("#coverImage")
 const addBookSubmit = document.querySelector("#addBookSubmit")
+const checkboxes = document.querySelectorAll(".checkbox")
 
 const library = []
 
@@ -32,22 +33,22 @@ function addBookToLibrary(book) {
 }
 
 function displayLibrary() {
+  console.log(library)
   libraryDisplay.innerHTML = ""
   library.forEach((book, index) => {
     const bookElement = document.createElement("div")
     bookElement.classList.add("book")
     bookElement.dataset.index = index
-    bookElement.style.backgroundImage = book.cover
+    bookElement.style.backgroundImage = `url("${book.cover}"`
 
-    bookElement.innerHTML = `<h2>${
+    bookElement.innerHTML = `<div class="checkbox"><img class="checkmark ${
+      book.read ? "read" : ""
+    }" src=${checkmark}></div><div class="bookInfo"><h2>${
       book.title
     }</h2><span class="author">Author: ${
       book.author
-    }</span><br><span class="pages">Pages: ${
-      book.pages
-    }</span><div class="checkbox"><img class="checkmark ${
-      book.read ? "read" : ""
-    }" src=${checkmark}></div>`
+    }</span><span class="pages">Pages: ${book.pages}</span>
+    </div>`
 
     libraryDisplay.appendChild(bookElement)
   })
@@ -58,77 +59,70 @@ const book1 = new Book(
   "Antoine de Saint-Exupéry",
   96,
   true,
-  "https://lorem.picsum/150/200"
+  "https://www.heel-verlag.de/cosmoshop/default/pix/a/g/9783755300281.3.jpg"
 )
 const book2 = new Book(
   "Stolz und Vorurteil",
   "Jane Austen",
   432,
   false,
-  "https://lorem.picsum/150/200"
+  "https://m.media-amazon.com/images/I/81Q-XHEQjAL._UF1000,1000_QL80_.jpg"
 )
 const book3 = new Book(
   "1984",
   "George Orwell",
   328,
   false,
-  "https://lorem.picsum/150/200"
+  "https://m.media-amazon.com/images/I/71rpa1-kyvL.jpg"
 )
 const book4 = new Book(
   "Der Name der Rose",
   "Umberto Eco",
   550,
   false,
-  "https://lorem.picsum/150/200"
+  "https://upload.wikimedia.org/wikipedia/de/thumb/5/5a/Der_Name_der_Rose_-_Hanser_1982.jpg/1200px-Der_Name_der_Rose_-_Hanser_1982.jpg"
 )
-const book5 = new Book(
-  "Harry Potter und der Stein der Weisen",
-  "J.K. Rowling",
-  309,
-  true,
-  "https://lorem.picsum/150/200"
-)
+
 const book6 = new Book(
   "Der Alchimist",
   "Paulo Coelho",
   197,
   true,
-  "https://lorem.picsum/150/200"
+  "https://m.media-amazon.com/images/I/61IRCUvPQVL.jpg"
 )
 const book7 = new Book(
   "Frankenstein",
   "Mary Shelley",
   216,
   false,
-  "https://lorem.picsum/150/200"
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Frankenstein_poster_1931.jpg/640px-Frankenstein_poster_1931.jpg"
 )
 const book8 = new Book(
   "Der Herr der Ringe",
   "J.R.R. Tolkien",
   1216,
   false,
-  "https://lorem.picsum/150/200"
+  "https://m.media-amazon.com/images/I/81r7sjdm5ML._AC_UF894,1000_QL80_.jpg"
 )
 const book9 = new Book(
   "Alice im Wunderland",
   "Lewis Carroll",
   219,
   true,
-  "https://lorem.picsum/150/200"
+  "https://images.thalia.media/-/BF2000-2000/13c5d43fbfcd40c297925e6114878c93/alice-im-wunderland-dvd-johnny-depp.jpeg"
 )
 const book10 = new Book(
   "Die unendliche Geschichte",
   "Michael Ende",
   528,
   true,
-  "https://lorem.picsum/150/200"
+  "https://michaelende.de/sites/default/files/Unendliche%20Geschichte_6.jpg"
 )
 
 addBookToLibrary(book1)
 addBookToLibrary(book2)
 addBookToLibrary(book3)
 addBookToLibrary(book4)
-addBookToLibrary(book5)
 addBookToLibrary(book6)
 addBookToLibrary(book7)
 addBookToLibrary(book8)
