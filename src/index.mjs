c0d37edd8e278da597902d6e792705490f6461d2
Sandro Hatzfeld <69820567+SandroHatzfeld@users.noreply.github.com>
2025-01-21 09:10:28 +0000

    Initial Setup and creating the Book Object

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,17 @@
-import "./styles.css";
+import "./styles.css"
 
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+
+  this.info = () => {
+    return `${this.title} by ${this.author} has ${this.pages} pages, you ${
+      this.read ? "already read it" : "didn't read it"
+    }.`
+  }
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+console.log(theHobbit.info())
